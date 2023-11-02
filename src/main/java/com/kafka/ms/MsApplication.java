b6ec2ff2bee13b27a2e6b4d3f0f436fffccf0552
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-02 00:14:09 -0300

    added a kafka topic

--- src/main/java/com/kafka/ms/MsApplication.java
+++ src/main/java/com/kafka/ms/MsApplication.java
@@ -1,13 +1,23 @@
 package com.kafka.ms;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class MsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner (KafkaTemplate<String,String> kafkaTemplate) {
+		return args -> {
+		kafkaTemplate.send("amy","HI BITCHES!");
+		};
 	}
 
 }
